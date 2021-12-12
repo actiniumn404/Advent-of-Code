80d11b2f795bbca30b663954f600f66ab019e504
Andrew C <83673744+actiniumn404@users.noreply.github.com>
2021-12-11 21:39:19 -0800

    Create day 11 part 1.py

---
+++ day 11 part 1.py
@@ -0,0 +1,33 @@
+data = open('inputs/day 11.txt').read().split("\n")
+matrix = []
+flashes = 0
+for x in data:
+    matrix.append(list(map(int, list(x))))
+
+def flash(y,x):
+    if (y,x) in flashed:
+        return
+    flashed.add((y,x))
+    global flashes
+    flashes += 1
+    matrix[y][x] = 0
+    for new_x, new_y in [(1,0), (-1, 0), (0, 1), (0, -1), (-1, 1), (1, 1), (-1, -1), (1, -1)]:
+        if y+new_y < 0 or x+new_x < 0 or y+new_y >= len(matrix) or x+new_x >= len(matrix[0]):
+            continue
+        if (y+new_y,x+new_x) not in flashed:
+            matrix[y+new_y][x+new_x] += 1
+        if matrix[y+new_y][x+new_x] > 9:
+            flash(y+new_y, x+new_x)
+
+for s in range(100):
+    flashed = set()
+    buffer = []
+    for y in range(len(matrix)):
+        for x in range(len(matrix[0])):
+            matrix[y][x] += 1
+            if matrix[y][x] > 9:
+                buffer.append([y,x])
+    for y, x in buffer:
+        flash(y, x)
+
+print(flashes)
